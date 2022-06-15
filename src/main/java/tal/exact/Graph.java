54d7a7a949c680ea6b98c2b0a7d404cec7125354
daronek6 <daro.pietrus@gmail.com>
2022-06-15 13:30:22 +0200

    - Added memory counter for heuristic algorith (LF) - fixed colorsVector

--- src/main/java/tal/exact/Graph.java
+++ src/main/java/tal/exact/Graph.java
@@ -121,7 +121,6 @@ public class Graph {
                 if(adjacencyMatrix[nodeId][i] == 1) {
                     usedColors.add(colorsVector[i]);
                     findUsedColorsCounter++;
-
                 }
             }
 
