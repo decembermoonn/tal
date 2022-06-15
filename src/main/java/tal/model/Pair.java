500088b4c421f64f5d63e9d6b8bfc91cab6b0f13
daronek6 <daro.pietrus@gmail.com>
2022-06-15 12:01:42 +0200

    Added heuristic algorithm (LF) and counter to Graph

---
+++ src/main/java/tal/model/Pair.java
@@ -0,0 +1,19 @@
+package tal.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.Comparator;
+
+@ToString
+@Getter
+@Setter
+@AllArgsConstructor
+public class Pair {
+
+    private int key;
+    private int value;
+
+}
